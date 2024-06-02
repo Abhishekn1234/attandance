648b58c2bc581644a6dbe37d236dcb4af99efc6f
Anas <anasmuhammed444@gmail.com>
2024-06-02 17:11:21 +0530

    commit

--- user.js
+++ user.js
@@ -113,33 +113,7 @@ try {
     }
 });
 
-// router.post('/admin-login', async (req, res) => {
-//     try {
-//         const { email, password } = req.body;
 
-    
-//         const user = await User.findOne({ email });
-//        console.log(user);
-      
-//         if (!user) {
-//             return res.status(401).json({ message: 'Invalid email or not an admin' });
-//         }
-
-       
-//         const isMatch = await bcrypt.compare(password, user.password);
-//         console.log(isMatch);
-//         console.log(password,user.password)
-//         if (!isMatch) {
-//             return res.status(401).json({ message: 'Invalid password' });
-//         }
-
-//         // If authentication succeeds, return a success response
-//         res.status(200).json({ message: 'Admin login successful', user, isAdmin: true });
-//     } catch (error) {
-//         console.error('Error during admin login:', error);
-//         res.status(500).json({ message: 'Internal server error' });
-//     }
-// });
 router.post("/login", async (req, res) => {
     try {
       const { email, password } = req.body;
